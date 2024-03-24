067606506ed18187183f863cf22ad7a0c7140786
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-03-24 14:51:29 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -65,13 +65,14 @@ async def cb_handler(client, query: CallbackQuery):
                 InlineKeyboardButton('💰 Donate', callback_data='donate')
                 ]])
         )
+        
 if data == "donate":
+    global START_PIC
     if Config.START_PIC:
         await query.message.reply_photo(Config.START_PIC, caption=Txt.START_TXT.format(user.mention), reply_markup=button)       
     else:
         await query.message.reply_text(text=Txt.START_TXT.format(user.mention), reply_markup=button, disable_web_page_preview=True)  
 
-    global Config.START_PIC
     Config.START_PIC = "https://graph.org/file/5876f43b347e9fd0125b5.jpg"
     await query.message.edit_text(
         text=Txt.DONATE_TXT,
@@ -81,6 +82,7 @@ if data == "donate":
             InlineKeyboardButton("🔙 Back", callback_data="help")
         ]])          
     )
+
 
     elif data == "file_names":
         format_template = await madflixbotz.get_format_template(user_id)
